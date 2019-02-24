50982a9cef53c21e4f44d9f5ff48e43929112e79
Stefan Fejes <ns.fejes.stefan@gmail.com>
2019-02-24 17:27:59 +0100

    add contributors link to footer

--- website/js/components/Footer.js
+++ website/js/components/Footer.js
@@ -2,6 +2,6 @@ import { h } from "hyperapp"
 
 export default () => (
     <footer>
-        <p>Made with ❤️ by <a href="https://stefanfejes.com">Stefan Fejes</a> & <a href="">contributors</a></p>
+        <p>Made with ❤️ by <a href="https://stefanfejes.com">Stefan Fejes</a> & <a href="https://github.com/fejes713/accessibility-guide/graphs/contributors">contributors</a></p>
     </footer>
 )
