9769799ecc24cc3529dd69d3738a803ff903ae9e
Stefan Fejes <ns.fejes.stefan@gmail.com>
2019-02-24 17:37:29 +0100

    conditionaly display resources

--- website/js/components/Card.js
+++ website/js/components/Card.js
@@ -16,16 +16,16 @@ export default ({
         id={name.slice(0, name.length - 3)}
     >
         <span class={`category ${category.replace(/\s/g, "-")}`}>{category}</span>
-    <Markdown node="h2" class="short-tip" content={shortTip} />
-    <Markdown node="p" class="long-tip" content={longTip} />
-    <section>
-        <p class="resource-title">Resources</p>
-        <ul class="resource-list">
-            {links.map(link => (
-                <Markdown node="li" class="resource" content={link} />
-            ))}
-        </ul>
-    </section>
+        <Markdown node="h2" class="short-tip" content={shortTip} />
+        <Markdown node="p" class="long-tip" content={longTip} />
+        {links.length > 0 && (<section>
+            <p class="resource-title">Resources</p>
+            <ul class="resource-list">
+                {links.map(link => (
+                    <Markdown node="li" class="resource" content={link} />
+                ))}
+            </ul>
+        </section>)}
 
     </li>
 )
